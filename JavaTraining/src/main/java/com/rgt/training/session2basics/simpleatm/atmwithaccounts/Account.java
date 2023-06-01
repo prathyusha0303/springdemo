93bd64cdea49a78df2ed270cbdb3bcc85e12d3e8
prathyusha0303 <134383759+prathyusha0303@users.noreply.github.com>
2023-06-01 11:30:22 +0530

    Account.java

--- JavaTraining/src/main/java/com/rgt/training/session2basics/simpleatm/atmwithaccounts/Account.java
+++ JavaTraining/src/main/java/com/rgt/training/session2basics/simpleatm/atmwithaccounts/Account.java
@@ -1,25 +1,32 @@
-package com.rgt.training.session2basics.simpleatm.atmwithaccounts;
+
+package com.rgt.training;
+
 public class Account {
-private int accountNumber;
-private int pin;
-private double balance;
+	private int accountNumber;
+	private int pin;
+	private double balance;
 
-public Account(int accountNumber, int pin, double balance) {
-super();
-this.accountNumber = accountNumber;
-this.pin = pin;
-this.balance = balance;
-}
+	public Account(int accountNumber, int pin, double balance) {
+		super();
+		this.accountNumber = accountNumber;
+		this.pin = pin;
+		this.balance = balance;
+	}
 
-public int getAccountNumber() {
-return accountNumber;
-}
+	public int getAccountNumber() {
+		return accountNumber;
+	}
 
-public int getPin() {
-return pin;
-}
+	public int getPin() {
+		return pin;
+	}
 
-public double getBalance() {
-return balance;
-}
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
 }
