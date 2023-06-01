ebac5c26a322c61d07acd8ea992ee0d9c02e75f6
prathyusha0303 <134383759+prathyusha0303@users.noreply.github.com>
2023-06-01 11:29:21 +0530

    Update ATMMachine.java

--- JavaTraining/src/main/java/com/rgt/training/session2basics/simpleatm/atmwithaccounts/ATMMachine.java
+++ JavaTraining/src/main/java/com/rgt/training/session2basics/simpleatm/atmwithaccounts/ATMMachine.java
@@ -1,107 +1,111 @@
-package com.rgt.training.session2basics.simpleatm.atmwithaccounts;
+package com.rgt.training;
 
 import java.util.Scanner;
 
-public class ATMMachine {
-	public static void main(String[] args) {
-		ATM atm = new ATM();
-		Scanner scanner = new Scanner(System.in);
-		Account[] accounts = new Account[10];
-		int choice;
-		int accountCount = 0;
+public class ATMProject {
+    public static void main(String[] args) {
+                ATM atm = new ATM();
+                Scanner scanner = new Scanner(System.in);
+                Account[] accounts = new Account[10];
+                int choice;
+                int accountCount = 0;
 
-		while (true) {
-			System.out.println("Welcome to the ATM");
-			System.out.println("1. Create Account");
-			System.out.println("2. Login");
-			System.out.println("3. Exit");
-			System.out.print("Choose an option: ");
-			choice = scanner.nextInt();
+                while (true) {
+                    System.out.println("Welcome to the ATM");
+                    System.out.println("1. Create Account");
+                    System.out.println("2. Login");
+                    System.out.println("3. Exit");
+                    System.out.print("Choose an option: ");
+                    choice = scanner.nextInt();
 
-			switch (choice) {
-			case 1:
-				if (accountCount >= accounts.length) {
-					System.out.println("Maximum number of accounts reached.");
-					break;
-				}
+                    switch (choice) {
+                        case 1:
+                            if (accountCount >= accounts.length) {
+                                System.out.println("Maximum number of accounts reached.");
+                                break;
+                            }
 
-				System.out.print("Enter account number: ");
-				int accountNumber = scanner.nextInt();
-				System.out.print("Enter PIN: ");
-				int pin = scanner.nextInt();
-				System.out.print("Enter initial balance: ");
-				double balance = scanner.nextDouble();
+                            System.out.print("Enter account number: ");
+                            int accountNumber = scanner.nextInt();
+                            System.out.print("Enter PIN: ");
+                            int pin = scanner.nextInt();
+                            System.out.print("Enter initial balance: ");
+                            double balance = scanner.nextDouble();
 
-				accounts[accountCount] = new Account(accountNumber, pin, balance);
-				System.out.println("Account created successfully!");
-				accountCount++;
-				break;
+                            accounts[accountCount] = new Account(accountNumber, pin, balance);
+                            System.out.println("Account created successfully!");
+                            accountCount++;
+                            break;
 
-			case 2:
-				System.out.print("Enter account number: ");
-				int accountNumberInput = scanner.nextInt();
-				System.out.print("Enter PIN: ");
-				int pinNumberInput = scanner.nextInt();
-				Account currentAccount = null;
-				for (int i = 0; i < accountCount; i++) {
-					Account account = accounts[i];
-					if (account.getAccountNumber() == accountNumberInput && account.getPin() == pinNumberInput) {
-						currentAccount = account;
-						break;
-					}
-				}
+                        case 2:
+                            System.out.print("Enter account number: ");
+                            int accountNumberInput = scanner.nextInt();
+                            System.out.print("Enter PIN: ");
+                            int pinNumberInput = scanner.nextInt();
+                            Account currentAccount = null;
+                            for (int i = 0; i < accountCount; i++) {
+                                Account account = accounts[i];
+                                if (account.getAccountNumber() == accountNumberInput && account.getPin() == pinNumberInput) {
+                                    currentAccount = account;
+                                    break;
+                                }
+                            }
 
-				if (currentAccount != null) {
-					System.out.println("Login successful!");
+                            if (currentAccount != null) {
+                                System.out.println("Login successful!");
 
-					do {
-						System.out.println("Welcome to the ATM");
-						System.out.println("1. Check Balance");
-						System.out.println("2. Deposit");
-						System.out.println("3. Withdraw");
-						System.out.println("4. Exit");
-						System.out.print("Choose an option: ");
-						choice = scanner.nextInt();
+                                do {
+                                    System.out.println("Welcome to the ATM");
+                                    System.out.println("1. Check Balance");
+                                    System.out.println("2. Deposit");
+                                    System.out.println("3. Withdraw");
+                                    System.out.println("4. Exit");
+                                    System.out.print("Choose an option: ");
+                                    choice = scanner.nextInt();
 
-						switch (choice) {
-						case 1:
-							System.out.println("Current balance: $" + currentAccount.getBalance());
-							break;
-						case 2:
-							System.out.print("Enter deposit amount: $");
-							double depositAmount = scanner.nextDouble();
-							atm.deposit(currentAccount, depositAmount);
-							break;
-						case 3:
-							System.out.print("Enter withdrawal amount: $");
-							double withdrawalAmount = scanner.nextDouble();
-							atm.withdraw(currentAccount, withdrawalAmount);
-							break;
-						case 4:
-							System.out.println("Goodbye!");
-							break;
-						default:
-							System.out.println("Invalid option. Please try again.");
-						}
-						System.out.println();
+                                    switch (choice) {
+                                        case 1:
+                                            System.out.println("Current balance: $" + currentAccount.getBalance());
+                                            break;
+                                        case 2:
+                                            System.out.print("Enter deposit amount: $");
+                                            double depositAmount = scanner.nextDouble();
+                                            double balanceamount=currentAccount.getBalance();
+                                            double bb=balanceamount+depositAmount;
+                                            currentAccount.setBalance(bb);
+                                            atm.deposit(currentAccount, depositAmount);
+                                            break;
+                                        case 3:
+                                            System.out.print("Enter withdrawal amount: $");
+                                            double withdrawalAmount = scanner.nextDouble();
+                                            currentAccount.setBalance(currentAccount.getBalance()-withdrawalAmount);
+                                            atm.withdraw(currentAccount, withdrawalAmount);
+                                            break;
+                                        case 4:
+                                            System.out.println("Goodbye!");
+                                            break;
+                                        default:
+                                            System.out.println("Invalid option. Please try again.");
+                                    }
+                                    System.out.println();
 
-					} while (choice != 4);
-				} else {
-					System.out.println("Invalid account number or PIN.");
-				}
-				break;
+                                } while (choice != 4);
+                            } else {
+                                System.out.println("Invalid account number or PIN.");
+                            }
+                            break;
 
-			case 3:
-				System.out.println("Goodbye!");
-				scanner.close();
-				System.exit(0);
-				break;
+                        case 3:
+                            System.out.println("Logout successfully");
+                            scanner.close();
+                            System.exit(0);
+                            break;
 
-			default:
-				System.out.println("Invalid option. Please try again.");
-			}
+                        default:
+                            System.out.println("Invalid option. Please try again.");
+                    }
 
-			System.out.println();
-		}
-	}
-}
+                    System.out.println();
+                }
+            }
+        }
