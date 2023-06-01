8fcf9ded5358ea3999438b20d51df21e146903a5
prathyusha0303 <134383759+prathyusha0303@users.noreply.github.com>
2023-06-01 11:28:51 +0530

    ATM.java

--- JavaTraining/src/main/java/com/rgt/training/session2basics/simpleatm/atmwithaccounts/ATM.java
+++ JavaTraining/src/main/java/com/rgt/training/session2basics/simpleatm/atmwithaccounts/ATM.java
@@ -1,28 +1,35 @@
-package com.rgt.training.session2basics.simpleatm.atmwithaccounts;
+package com.rgt.training;
+
 public class ATM {
-private double balance;
-public ATM() {
-balance = 0;
-}
-public double checkBalance() {
-return balance;
-}
-public void deposit(Account currentAccount, double amount) {
-if (amount > 0) {
-balance += amount;
-System.out.println("Deposit successful!");
-} else {
-System.out.println("Invalid deposit amount.");
-}
-}
-public void withdraw(Account currentAccount, double amount) {
-if (amount > balance) {
-System.out.println("Insufficient balance.");
-} else if (amount <= 0) {
-System.out.println("Invalid withdrawal amount.");
-} else {
-balance -= amount;
-System.out.println("Withdrawal successful!");
-}
-}
+	
+	private double balance;
+
+	public ATM() {
+		balance = 0;
+	}
+
+	public double checkBalance() {
+		return balance;
+	}
+
+	public void deposit(Account currentAccount, double amount) {
+		if (amount > 0) {
+			Double balanceamount=currentAccount.getBalance();
+			balance =balanceamount+amount;
+			System.out.println("Deposit successful!");
+		} else {
+			System.out.println("Invalid deposit amount.");
+		}
+	}
+
+	public void withdraw(Account currentAccount, double amount) {
+		if (amount > balance) {
+			System.out.println("Insufficient balance.");
+		} else if (amount <= 0) {
+			System.out.println("Invalid withdrawal amount.");
+		} else {
+			balance -= amount;
+			System.out.println("Withdrawal successful!");
+		}
+	}
 }
